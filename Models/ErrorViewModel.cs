833b1061dccd649b68f92d45843d32b607e00d97
Dennise Catolos <dentolos19@gmail.com>
2025-11-11 23:57:58 +0800

    Added project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreshFarmMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
