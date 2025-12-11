d282d64a5a6abba685914bfe8616cd45433572a3
Dennise Catolos <dentolos19@gmail.com>
2025-12-11 12:55:20 +0800

    Updated project

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,7 @@
-namespace FreshFarmMarket.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace FreshFarmMarket.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
